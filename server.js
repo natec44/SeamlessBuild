adec4c9a2ef29ad8d88887e3db5b5cff59adeafb
natec44 <christophersen44@gmail.com>
2025-05-29 10:33:00 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,44 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import http from 'http';
+import { Server as SocketIOServer } from 'socket.io';
 
+import projectRoutes from './routes/project.js';
+import taskRoutes from './routes/task.js';
+import userRoutes from './routes/user.js';
+import vendorRoutes from './routes/vendor.js';
+import analyticsRoutes from './routes/analytics.js';
+import notificationsRoutes from './routes/notifications.js';
+import adminRoutes from './routes/admin.js';
+import { errorHandler } from './middleware/errorHandler.js';
+import { initSocket } from './socket.js';
+
+dotenv.config();
+const app = express();
+const server = http.createServer(app);
+const io = new SocketIOServer(server, { cors: { origin: '*' } });
+initSocket(io);
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/projects', projectRoutes);
+app.use('/api/tasks', taskRoutes);
+app.use('/api/users', userRoutes);
+app.use('/api/vendors', vendorRoutes);
+app.use('/api/analytics', analyticsRoutes);
+app.use('/api/notifications', notificationsRoutes);
+app.use('/api/admin', adminRoutes);
+
+app.use(errorHandler);
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('MongoDB connected');
+    server.listen(process.env.PORT || 5000, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => console.error(err));
