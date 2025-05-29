8bd13ab9f66102bad3b76a3011cbf1209813ea3f
natec44 <christophersen44@gmail.com>
2025-05-29 10:48:11 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
+import React from 'react';
+import Dashboard from './components/Dashboard';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import AdminDashboard from './components/AdminDashboard';
+import NotificationsPanel from './components/NotificationsPanel';
 
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="/notifications" element={<NotificationsPanel />} />
+        {/* Add more routes as needed */}
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
