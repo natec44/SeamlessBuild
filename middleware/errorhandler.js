60e391123174b0a0bb13620dd8aa9e34914d3cad
natec44 <christophersen44@gmail.com>
2025-05-29 10:18:11 -0700

    Update errorhandler.js

--- middleware/errorhandler.js
+++ middleware/errorhandler.js
@@ -1,1 +1,8 @@
-
+module.exports = (err, req, res, next) => {
+  console.error(err);
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode).json({
+    message: err.message,
+    stack: process.env.NODE_ENV === 'production' ? '🥞' : err.stack
+  });
+};
