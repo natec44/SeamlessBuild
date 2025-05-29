eca95b84accbc4ee60056b19b4513c9ee943138b
natec44 <christophersen44@gmail.com>
2025-05-29 09:57:30 -0700

    Update projects.js

--- routes/projects.js
+++ routes/projects.js
@@ -1,1 +1,21 @@
+// server/routes/projects.js
+const express = require('express');
+const router = express.Router();
+const projectController = require('../controllers/projects');
 
+// GET all projects
+router.get('/', projectController.getAllProjects);
+
+// GET single project
+router.get('/:id', projectController.getProjectById);
+
+// POST create new project
+router.post('/', projectController.createProject);
+
+// PUT update project
+router.put('/:id', projectController.updateProject);
+
+// DELETE project
+router.delete('/:id', projectController.deleteProject);
+
+module.exports = router;
