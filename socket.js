0189a33b50a7254aaaf271937e4e86db1b178029
natec44 <christophersen44@gmail.com>
2025-05-29 10:33:27 -0700

    Update socket.js

--- socket.js
+++ socket.js
@@ -1,1 +1,24 @@
+let ioInstance;
 
+export const initSocket = (io) => {
+  ioInstance = io;
+
+  io.on('connection', (socket) => {
+    console.log(`[Socket] Connected: ${socket.id}`);
+
+    socket.on('joinProject', (projectId) => {
+      socket.join(projectId);
+      console.log(`[Socket] ${socket.id} joined project ${projectId}`);
+    });
+
+    socket.on('sendUpdate', ({ projectId, data }) => {
+      socket.to(projectId).emit('receiveUpdate', data);
+    });
+
+    socket.on('disconnect', () => {
+      console.log(`[Socket] Disconnected: ${socket.id}`);
+    });
+  });
+};
+
+export const getIO = () => ioInstance;
