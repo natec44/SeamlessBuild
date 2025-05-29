73db75b839f70fb1dfb5aae44abef3415a0f44fe
natec44 <christophersen44@gmail.com>
2025-05-29 10:04:17 -0700

    Update vendors.js

--- routes/vendors.js
+++ routes/vendors.js
@@ -1,1 +1,18 @@
+// server/routes/vendors.js
+const express = require('express');
+const router = express.Router();
+const vendorController = require('../controllers/vendors');
 
+// GET all vendors for a project
+router.get('/:projectId', vendorController.getVendors);
+
+// POST add a new vendor
+router.post('/', vendorController.addVendor);
+
+// PUT update a vendor
+router.put('/:vendorId', vendorController.updateVendor);
+
+// DELETE a vendor
+router.delete('/:vendorId', vendorController.deleteVendor);
+
+module.exports = router;
