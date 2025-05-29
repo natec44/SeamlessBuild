db02f38167a0f14b58e1e16d234ee0cbd13fe0e3
natec44 <christophersen44@gmail.com>
2025-05-29 10:48:24 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import * as serviceWorkerRegistration from './service-worker';
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+serviceWorkerRegistration.register();
