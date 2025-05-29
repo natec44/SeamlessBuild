9abfdb1d8fac32e748356ab117b418067776f510
natec44 <christophersen44@gmail.com>
2025-05-29 10:42:02 -0700

    Update ganttchart.jsx

--- src/components/ganttchart.jsx
+++ src/components/ganttchart.jsx
@@ -1,1 +1,12 @@
+import React from 'react';
 
+const GanttChart = () => {
+  return (
+    <div className="p-4 col-span-1 xl:col-span-2">
+      <h2 className="text-lg font-semibold mb-2">Gantt Chart</h2>
+      <p>This section will visualize the project timeline with a Gantt chart.</p>
+    </div>
+  );
+};
+
+export default GanttChart;
