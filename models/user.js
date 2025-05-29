e1f07364a7725c3ea10f4011d1c17be52fdd0e09
natec44 <christophersen44@gmail.com>
2025-05-29 10:13:23 -0700

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,1 +1,22 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
+const UserSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  role: { type: String, enum: ['admin', 'project_manager', 'team_member', 'vendor'], default: 'team_member' },
+  createdAt: { type: Date, default: Date.now }
+});
+
+UserSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+UserSchema.methods.comparePassword = function(candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
