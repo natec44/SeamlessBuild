439813f7e8468f693613d709a7c53beb1c1b4296
natec44 <christophersen44@gmail.com>
2025-05-29 10:08:23 -0700

    Update notifications.js

--- routes/notifications.js
+++ routes/notifications.js
@@ -1,1 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const notificationController = require('../controllers/notifications');
 
+// GET user notifications
+router.get('/', notificationController.getNotifications);
+
+// POST create notification
+router.post('/', notificationController.createNotification);
+
+// PUT mark as read
+router.put('/:id', notificationController.markAsRead);
+
+// DELETE notification
+router.delete('/:id', notificationController.deleteNotification);
+
+module.exports = router;
