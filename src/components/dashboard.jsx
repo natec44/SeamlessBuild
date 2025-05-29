8783561cf03c95b4f25ac4f222f0ec74a1c75811
natec44 <christophersen44@gmail.com>
2025-05-29 10:41:50 -0700

    Update dashboard.jsx

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,1 +1,22 @@
+import React from 'react';
+import ProjectSelector from './ProjectSelector';
+import TaskTracking from './TaskTracking';
+import MaterialsVendorPanel from './MaterialsVendorPanel';
+import NotificationsPanel from './NotificationsPanel';
+import AnalyticsPanel from './AnalyticsPanel';
+import GanttChart from './GanttChart';
 
+const Dashboard = () => {
+  return (
+    <div className="p-4 grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
+      <ProjectSelector />
+      <TaskTracking />
+      <MaterialsVendorPanel />
+      <NotificationsPanel />
+      <AnalyticsPanel data={[]} />
+      <GanttChart />
+    </div>
+  );
+};
+
+export default Dashboard;
