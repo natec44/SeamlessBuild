a8cfec6c0447a987711b67c3130ab2312b02f249
natec44 <christophersen44@gmail.com>
2025-05-29 10:08:07 -0700

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -1,1 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const adminController = require('../controllers/admin');
 
+// GET all users
+router.get('/users', adminController.getAllUsers);
+
+// PUT update user role
+router.put('/users/:id/role', adminController.updateUserRole);
+
+// DELETE user
+router.delete('/users/:id', adminController.deleteUser);
+
+// GET system metrics
+router.get('/metrics', adminController.getSystemMetrics);
+
+module.exports = router;
