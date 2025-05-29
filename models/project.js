1673b4332aa752bc6160aa83ba2a6616f6643ebc
natec44 <christophersen44@gmail.com>
2025-05-29 10:12:51 -0700

    Update project.js

--- models/project.js
+++ models/project.js
@@ -1,1 +1,14 @@
+const mongoose = require('mongoose');
 
+const ProjectSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: String,
+  startDate: Date,
+  endDate: Date,
+  location: String,
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  team: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Project', ProjectSchema);
