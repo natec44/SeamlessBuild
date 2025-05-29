e12426b665559cd6574074a1bd07127252a2d87b
natec44 <christophersen44@gmail.com>
2025-05-29 09:56:17 -0700

    Update tasks.js

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,1 +1,39 @@
+// server/controllers/tasks.js
+const Task = require('../models/Task');
 
+exports.getTasks = async (req, res, next) => {
+  try {
+    const tasks = await Task.find({ projectId: req.params.projectId });
+    res.json(tasks);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.createTask = async (req, res, next) => {
+  try {
+    const newTask = new Task(req.body);
+    await newTask.save();
+    res.status(201).json(newTask);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.updateTask = async (req, res, next) => {
+  try {
+    const updatedTask = await Task.findByIdAndUpdate(req.params.taskId, req.body, { new: true });
+    res.json(updatedTask);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.deleteTask = async (req, res, next) => {
+  try {
+    await Task.findByIdAndDelete(req.params.taskId);
+    res.status(204).end();
+  } catch (error) {
+    next(error);
+  }
+};
