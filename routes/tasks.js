d0d69f9d1b22ff4709be54f83553607c4ddbf5a0
natec44 <christophersen44@gmail.com>
2025-05-29 09:55:49 -0700

    Update tasks.js

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,1 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const taskController = require('../controllers/tasks');
 
+// GET all tasks for a project
+router.get('/:projectId', taskController.getTasks);
+
+// POST a new task
+router.post('/', taskController.createTask);
+
+// PUT update a task
+router.put('/:taskId', taskController.updateTask);
+
+// DELETE a task
+router.delete('/:taskId', taskController.deleteTask);
+
+module.exports = router;
