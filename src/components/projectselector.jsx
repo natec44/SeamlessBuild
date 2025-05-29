80dc4cc8cf0bc4bb1e1b61d56f6c8027f3170678
natec44 <christophersen44@gmail.com>
2025-05-29 10:44:26 -0700

    Update projectselector.jsx

--- src/components/projectselector.jsx
+++ src/components/projectselector.jsx
@@ -1,1 +1,15 @@
+import React from 'react';
 
+const ProjectSelector = () => {
+  return (
+    <div className="p-4">
+      <h2 className="text-lg font-semibold mb-2">Select Project</h2>
+      <select className="w-full border rounded p-2">
+        <option value="">-- Select a Project --</option>
+        {/* Map your projects here */}
+      </select>
+    </div>
+  );
+};
+
+export default ProjectSelector;
