97b0bef6ade87d0d71cf2992e7c5612f8a855ef2
natec44 <christophersen44@gmail.com>
2025-05-29 09:57:52 -0700

    Update projects.js

--- controllers/projects.js
+++ controllers/projects.js
@@ -1,1 +1,49 @@
+// server/controllers/projects.js
+const Project = require('../models/Project');
 
+exports.getAllProjects = async (req, res, next) => {
+  try {
+    const projects = await Project.find({ userId: req.user.id });
+    res.json(projects);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.getProjectById = async (req, res, next) => {
+  try {
+    const project = await Project.findById(req.params.id);
+    if (!project) return res.status(404).json({ message: 'Project not found' });
+    res.json(project);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.createProject = async (req, res, next) => {
+  try {
+    const newProject = new Project({ ...req.body, userId: req.user.id });
+    await newProject.save();
+    res.status(201).json(newProject);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.updateProject = async (req, res, next) => {
+  try {
+    const updated = await Project.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(updated);
+  } catch (error) {
+    next(error);
+  }
+};
+
+exports.deleteProject = async (req, res, next) => {
+  try {
+    await Project.findByIdAndDelete(req.params.id);
+    res.status(204).end();
+  } catch (error) {
+    next(error);
+  }
+};
